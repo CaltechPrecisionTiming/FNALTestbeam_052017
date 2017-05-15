b1f40903fb70b41f290902a84dd090e711606d00
Si Xie <si.xie@cern.ch>
2017-05-14 21:36:33 -0500

    update script

--- processNewRuns.sh
+++ processNewRuns.sh
@@ -5,5 +5,5 @@ sdiff totalList doneList | grep "<" | awk '{print $1}' >! newList
 foreach f(`cat newList`)
    echo "Processing Run${f}"
    echo "./dat2rootPixels /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/RawDataSaver0CMSTiming0_Run${f}_0_Raw.dat /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/Run${f}_CMSTiming_converted.root /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/RECO/Run${f}_RECO.root -1"
-   ./dat2rootPixels /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/RawDataSaver0CMSTiming0_Run${f}_0_Raw.dat /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/Run${f}_CMSTiming_converted.root /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/RECO/Run${f}_RECO.root 
+   ./dat2rootPixels /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/RawDataSaver0CMSTiming0_Run${f}_0_Raw.dat /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/Run${f}_CMSTiming_converted.root /eos/uscms/store/user/cmstestbeam/ETL/MT6Section1Data/052017/RECO/Run${f}_RECO.root -1
 end

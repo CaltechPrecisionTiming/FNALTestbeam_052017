a792d664fce0ab8f3f3b1a63268d0176d7201ab7
Si Xie <si.xie@cern.ch>
2017-05-14 22:44:15 -0500

    add eff measurement

--- Analysis/LGAD/pulse.cc
+++ Analysis/LGAD/pulse.cc
@@ -3,6 +3,7 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include "EfficiencyUtils.hh"
 
 void pulse::Loop()
 {
@@ -41,3 +42,64 @@ void pulse::Loop()
       // if (Cut(ientry) < 0) continue;
    }
 }
+
+
+void pulse::MakeEfficiencyVsXY(int channelNumber) {
+
+  //declare histograms
+  TH1F *histX_den = new TH1F("histX_den",";X [mm];Number of Events"; 400, 20,30);
+  TH1F *histX_num = new TH1F("histX_num",";X [mm];Number of Events"; 400, 20,30);
+  TH1F *histY_den = new TH1F("histY_den",";Y [mm];Number of Events"; 400, 10,20);
+  TH1F *histY_num = new TH1F("histY_num",";Y [mm];Number of Events"; 400, 10,20);
+  
+  if (fChain == 0) return;
+   Long64_t nentries = fChain->GetEntriesFast();
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+
+      //cuts
+
+      //require photek to show MIP signal
+      if (!(amp[0] > 0.1 && amp[0] < 0.2)) continue;
+
+      histX_den->Fill( 0.001*x2 );
+      histY_den->Fill( 0.001*y2 );
+      
+      //numerator
+      if (amp[channelNumber] > 0.08) {
+	histX_num->Fill( 0.001*x2 );
+	histY_num->Fill( 0.001*y2 );       
+      }
+   }
+
+   vector<double> xbins;
+   vector<double> ybins;
+   TGraphAsymmErrors* effX = createEfficiencyGraph(histX_num, histX_den,
+						   Form("EfficiencyVsX_Channel%d",channelNumber),
+						   xbins,
+						   11.5, 14.5,
+						   0.0, 1.0,
+						   false
+						   );
+   TGraphAsymmErrors* effY = createEfficiencyGraph(histY_num, histY_den,
+						   Form("EfficiencyVsY_Channel%d",channelNumber),
+						   ybins,
+						   25.5, 28.5,
+						   0.0, 1.0,
+						   false
+						   );
+   
+   TCanvas *cv = new TCanvas ("cv","cv", 800,800);
+
+   effX->Draw("AP");
+
+   
+
+
+}
+
+
+
--- Analysis/LGAD/pulse.hh
+++ Analysis/LGAD/pulse.hh
@@ -90,6 +90,8 @@ public :
    virtual void     Loop();
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
+   void MakeEfficiencyVsXY();
+
 };
 
 #endif

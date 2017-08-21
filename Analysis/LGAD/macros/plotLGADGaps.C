2f458556a2648a1a6c7dfc3cdf954a465fae33ad
SiXie <Si.Xie@cern.ch>
2017-08-21 23:09:27 +0200

    add gap eff plot macro and root files

--- Analysis/LGAD/macros/plotLGADGaps.C
+++ Analysis/LGAD/macros/plotLGADGaps.C
@@ -1,21 +1,122 @@
-void MakeGap_HPK() {
+void MakeGap_HPKVsX() {
 
-  TFile *_file1 = TFile::Open("Eff_vs_X_Ch1.root");
-  TFile *_file2 = TFile::Open("Eff_vs_X_Ch4.root");
+  TFile *_file1 = TFile::Open("eff_Channel1_Run638-781.root");
+  TFile *_file2 = TFile::Open("eff_Channel2_Run638-781.root");
   
-  TCanvas *c = new TCanvas("c","c",700,600);
+  TCanvas *c = new TCanvas("c","c",800,800);
   c->SetLeftMargin(0.13);
-  c->SetRightMargin(0.18);
+  c->SetRightMargin(0.05);
 
-  TCanvas *c1 = (TCanvas*)_file1->Get("canvas");
-  TCanvas *c2 = (TCanvas*)_file2->Get("canvas");
-  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)c1->GetPrimitive("Graph0");
-  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)c2->GetPrimitive("Graph0");
+  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)_file1->Get("Efficiency_X");
+  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)_file2->Get("Efficiency_X");
 
-  g2->SetMarkerColor(2);
-  g2->SetLineColor(2);
+  g1->SetMarkerSize(1.2);
+  g2->SetMarkerSize(1.2);
+  g1->SetMarkerColor(kBlue);
+  g1->SetLineColor(kBlue);
+  g2->SetMarkerColor(kRed);
+  g2->SetLineColor(kRed);
+  g1->SetLineWidth(2);
+  g2->SetLineWidth(2);
+  
+  g1->GetXaxis()->SetLabelSize(0.04);
+  g1->GetXaxis()->SetTitleSize(0.04);
+  g1->GetXaxis()->SetLabelOffset(0.02);
+  g1->GetXaxis()->SetTitleOffset(1.2);
+  g1->GetYaxis()->SetLabelSize(0.05);
+  g1->GetYaxis()->SetTitleSize(0.05);
+  g1->GetYaxis()->SetTitleOffset(1.1);
 
-  // gStyle->SetPalette(1); //kBird
+
+  c->Draw();
+
+  g1->Draw("ap");
+  g1->GetXaxis()->SetRangeUser(14.25,14.65);
+  g2->Draw("p same");
+  
+  TF1 * f1 = new TF1("f1", "[0]*TMath::Erf(([1]-x)/[2])+[3]", 14.25, 14.65);
+  f1->SetParameters(1., 14.45, 1, 0.);
+  g1->Fit("f1","R");
+
+  Double_t edge1 = f1->GetX(0.5);
+  f1->SetLineColor(kBlue);
+  f1->SetLineWidth(3);
+  f1->Draw("same");
+  std::cout<<edge1<<std::endl;
+
+  TF1 * f2 = new TF1("f2", "[0]*TMath::Erf((x-[1])/[2])+[3]", 14.25, 14.65);
+  f2->SetParameters(1., 14.55, 1, 0.);
+  g2->Fit("f2","R");
+  Double_t edge2 = f2->GetX(0.5);
+  f2->SetLineColor(kRed);
+  f2->SetLineWidth(3);
+  f2->Draw("same");
+
+
+  TArrow *ar3 = new TArrow(edge1,0.5,edge2,0.5,0.05,"<|>");
+  ar3->SetAngle(35);
+  ar3->SetLineWidth(2);
+  ar3->SetArrowSize(0.03);
+  ar3->SetLineColor(1);
+  ar3->SetFillColor(1);
+  ar3->Draw();
+
+  TLatex *   tex = new TLatex(0.49, 0.5,Form("%4.0f #mum",round(100*(edge2-edge1))*10));
+  tex->SetNDC();
+  tex->SetTextSize(0.05);
+  tex->Draw();
+
+  TLatex *   tex1 = new TLatex(0.2, 0.94,"LGAD Sensor: HPK 50D-PIX");
+  tex1->SetNDC();
+  tex1->SetTextSize(0.05);
+  tex1->Draw();  
+  
+  TLegend *leg = new TLegend(0.70,0.35,0.94,0.5,NULL,"brNDC");
+   leg->SetBorderSize(1);
+   leg->SetTextSize(0.04);
+   leg->SetLineColor(1);
+   leg->SetLineStyle(1);
+   leg->SetLineWidth(2);
+   leg->SetFillColor(0);
+   leg->SetFillStyle(1001);
+   leg->AddEntry(g1, "Channel 1", "LP");
+   leg->AddEntry(g2, "Channel 2", "LP");
+   leg->Draw();
+
+   c->SaveAs("Eff_vs_X_HPK50DPIX_Ch1_2_fit.pdf");
+   c->SaveAs("Eff_vs_X_HPK50DPIX_Ch1_2_fit.C");
+
+}
+
+void MakeGap_HPKVsX_UCSCBoard() {
+
+  TFile *_file1 = TFile::Open("eff_Channel1_Run838+839+842.root");
+  TFile *_file2 = TFile::Open("eff_Channel4_Run838+839+842.root");
+  
+  TCanvas *c = new TCanvas("c","c",800,800);
+  c->SetLeftMargin(0.13);
+  c->SetRightMargin(0.05);
+
+  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)_file1->Get("Efficiency_X");
+  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)_file2->Get("Efficiency_X");
+
+  g1->SetMarkerSize(1.2);
+  g2->SetMarkerSize(1.2);
+  g1->SetMarkerColor(kBlue);
+  g1->SetLineColor(kBlue);
+  g2->SetMarkerColor(kRed);
+  g2->SetLineColor(kRed);
+  g1->SetLineWidth(2);
+  g2->SetLineWidth(2);
+  
+  g1->GetXaxis()->SetLabelSize(0.04);
+  g1->GetXaxis()->SetTitleSize(0.04);
+  g1->GetXaxis()->SetLabelOffset(0.02);
+  g1->GetXaxis()->SetTitleOffset(1.2);
+  g1->GetYaxis()->SetLabelSize(0.05);
+  g1->GetYaxis()->SetTitleSize(0.05);
+  g1->GetYaxis()->SetTitleOffset(1.1);
+
 
   c->Draw();
 
@@ -28,12 +129,18 @@ void MakeGap_HPK() {
   g1->Fit("f1","R");
 
   Double_t edge1 = f1->GetX(0.5);
+  f1->SetLineColor(kBlue);
+  f1->SetLineWidth(3);
+  f1->Draw("same");
   std::cout<<edge1<<std::endl;
 
   TF1 * f2 = new TF1("f2", "[0]*TMath::Erf((x-[1])/[2])+[3]", 13.5, 13.9);
   f2->SetParameters(1., 13.6, 1, 0.);
   g2->Fit("f2","R");
   Double_t edge2 = f2->GetX(0.5);
+  f2->SetLineColor(kRed);
+  f2->SetLineWidth(3);
+  f2->Draw("same");
 
 
   TArrow *ar3 = new TArrow(edge1,0.5,edge2,0.5,0.05,"<|>");
@@ -44,7 +151,7 @@ void MakeGap_HPK() {
   ar3->SetFillColor(1);
   ar3->Draw();
 
-  TLatex *   tex = new TLatex(0.41, 0.5,Form("%4.2f #mum",1000*(edge2-edge1)));
+  TLatex *   tex = new TLatex(0.47, 0.5,Form("%4.0f #mum",round(100*(edge2-edge1))*10));
   tex->SetNDC();
   tex->SetTextSize(0.05);
   tex->Draw();
@@ -54,26 +161,53 @@ void MakeGap_HPK() {
   tex1->SetTextSize(0.05);
   tex1->Draw();  
   
+  TLegend *leg = new TLegend(0.70,0.35,0.94,0.5,NULL,"brNDC");
+   leg->SetBorderSize(1);
+   leg->SetTextSize(0.04);
+   leg->SetLineColor(1);
+   leg->SetLineStyle(1);
+   leg->SetLineWidth(2);
+   leg->SetFillColor(0);
+   leg->SetFillStyle(1001);
+   leg->AddEntry(g1, "Channel 1", "LP");
+   leg->AddEntry(g2, "Channel 4", "LP");
+   leg->Draw();
+
+   c->SaveAs("Eff_vs_X_HPK50DPIX_Ch1_4_fit.pdf");
+   c->SaveAs("Eff_vs_X_HPK50DPIX_Ch1_4_fit.C");
+
 }
 
-void MakeGap_HPKY() {
 
-  TFile *_file1 = TFile::Open("Eff_vs_Y_Ch3.root");
-  TFile *_file2 = TFile::Open("Eff_vs_Y_Ch4.root");
+
+void MakeGap_HPKVsY() {
+
+  TFile *_file1 = TFile::Open("eff_Channel3_Run838+839+842.root");
+  TFile *_file2 = TFile::Open("eff_Channel4_Run838+839+842.root");
   
-  TCanvas *c = new TCanvas("c","c",700,600);
+  TCanvas *c = new TCanvas("c","c",800,800);
   c->SetLeftMargin(0.13);
-  c->SetRightMargin(0.18);
+  c->SetRightMargin(0.05);
 
-  TCanvas *c1 = (TCanvas*)_file1->Get("canvas");
-  TCanvas *c2 = (TCanvas*)_file2->Get("canvas");
-  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)c1->GetPrimitive("Graph0");
-  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)c2->GetPrimitive("Graph0");
+  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)_file1->Get("Efficiency_Y");
+  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)_file2->Get("Efficiency_Y");
 
-  g2->SetMarkerColor(2);
-  g2->SetLineColor(2);
-
-  // gStyle->SetPalette(1); //kBird
+  g1->SetMarkerSize(1.2);
+  g2->SetMarkerSize(1.2);
+  g1->SetMarkerColor(kBlue);
+  g1->SetLineColor(kBlue);
+  g2->SetMarkerColor(kRed);
+  g2->SetLineColor(kRed);
+  g1->SetLineWidth(2);
+  g2->SetLineWidth(2);
+  
+  g1->GetXaxis()->SetLabelSize(0.04);
+  g1->GetXaxis()->SetTitleSize(0.04);
+  g1->GetXaxis()->SetLabelOffset(0.02);
+  g1->GetXaxis()->SetTitleOffset(1.2);
+  g1->GetYaxis()->SetLabelSize(0.05);
+  g1->GetYaxis()->SetTitleSize(0.05);
+  g1->GetYaxis()->SetTitleOffset(1.1);
 
   c->Draw();
 
@@ -81,17 +215,24 @@ void MakeGap_HPKY() {
   g1->GetXaxis()->SetRangeUser(21.8,22.2);
   g2->Draw("p same");
   
-  TF1 * f1 = new TF1("f1", "[0]*TMath::Erf(([1]-x)/[2])+[3]", 21.9, 22.2);
-  f1->SetParameters(1., 22., 1, 0.);
+  TF1 * f1 = new TF1("f1", "[0]*TMath::Erf(([1]-x)/[2])+[3]", 21.8, 22.2);
+  f1->SetParameters(1., 22.05, 1, 0.);
   g1->Fit("f1","R");
 
   Double_t edge1 = f1->GetX(0.5);
+  f1->SetLineColor(kBlue);
+  f1->SetLineWidth(3);
+  f1->Draw("same");
   std::cout<<edge1<<std::endl;
 
-  TF1 * f2 = new TF1("f2", "[0]*TMath::Erf((x-[1])/[2])+[3]", 21.8, 22.1);
-  f2->SetParameters(1., 22., 1, 0.);
+  TF1 * f2 = new TF1("f2", "[0]*TMath::Erf((x-[1])/[2])+[3]", 21.8, 22.2);
+  f2->SetParameters(1., 22.05, 1, 0.);
   g2->Fit("f2","R");
   Double_t edge2 = f2->GetX(0.5);
+  f2->SetLineColor(kRed);
+  f2->SetLineWidth(3);
+  f2->Draw("same");
+
 
 
   TArrow *ar3 = new TArrow(edge1,0.5,edge2,0.5,0.05,"<|>");
@@ -102,7 +243,7 @@ void MakeGap_HPKY() {
   ar3->SetFillColor(1);
   ar3->Draw();
 
-  TLatex *   tex = new TLatex(0.41, 0.5,Form("%4.2f #mum",1000*(edge1-edge2)));
+  TLatex *   tex = new TLatex(0.43, 0.5,Form("%4.0f #mum",round(100*(edge1-edge2))*10));
   tex->SetNDC();
   tex->SetTextSize(0.05);
   tex->Draw();
@@ -111,26 +252,55 @@ void MakeGap_HPKY() {
   tex1->SetNDC();
   tex1->SetTextSize(0.05);
   tex1->Draw();  
-  
+
+
+  TLegend *leg = new TLegend(0.70,0.35,0.94,0.5,NULL,"brNDC");
+   leg->SetBorderSize(1);
+   leg->SetTextSize(0.04);
+   leg->SetLineColor(1);
+   leg->SetLineStyle(1);
+   leg->SetLineWidth(2);
+   leg->SetFillColor(0);
+   leg->SetFillStyle(1001);
+   leg->AddEntry(g1, "Channel 3", "LP");
+   leg->AddEntry(g2, "Channel 4", "LP");
+   leg->Draw();
+
+   c->SaveAs("Eff_vs_Y_HPK50DPIX_Ch3_4_fit.pdf");
+   c->SaveAs("Eff_vs_Y_HPK50DPIX_Ch3_4_fit.C");
+
 }
 
 
-void MakeGap_CNM() {
+void MakeGap_CNMVsX() {
 
-  TFile *_file1 = TFile::Open("Eff_vs_X_Ch10.root");
-  TFile *_file2 = TFile::Open("Eff_vs_X_Ch13.root");
+  TFile *_file1 = TFile::Open("eff_Channel10_Run838+839+842.root");
+  TFile *_file2 = TFile::Open("eff_Channel13_Run838+839+842.root");
   
-  TCanvas *c = new TCanvas("c","c",700,600);
+  TCanvas *c = new TCanvas("c","c",800,800);
   c->SetLeftMargin(0.13);
-  c->SetRightMargin(0.18);
+  c->SetRightMargin(0.05);
 
-  TCanvas *c1 = (TCanvas*)_file1->Get("canvas");
-  TCanvas *c2 = (TCanvas*)_file2->Get("canvas");
-  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)c1->GetPrimitive("Graph0");
-  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)c2->GetPrimitive("Graph0");
+  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)_file1->Get("Efficiency_X");
+  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)_file2->Get("Efficiency_X");
 
-  g2->SetMarkerColor(2);
-  g2->SetLineColor(2);
+  g1->SetMarkerSize(1.2);
+  g2->SetMarkerSize(1.2);
+  g1->SetMarkerColor(kBlue);
+  g1->SetLineColor(kBlue);
+  g2->SetMarkerColor(kRed);
+  g2->SetLineColor(kRed);
+  g1->SetLineWidth(2);
+  g2->SetLineWidth(2);
+  
+  g1->GetXaxis()->SetLabelSize(0.04);
+  g1->GetXaxis()->SetTitleSize(0.04);
+  g1->GetXaxis()->SetLabelOffset(0.02);
+  g1->GetXaxis()->SetTitleOffset(1.2);
+  g1->GetYaxis()->SetLabelSize(0.05);
+  g1->GetYaxis()->SetTitleSize(0.05);
+  g1->GetYaxis()->SetTitleOffset(1.1);
+
 
   // gStyle->SetPalette(1); //kBird
 
@@ -146,11 +316,17 @@ void MakeGap_CNM() {
 
   Double_t edge1 = f1->GetX(0.5);
   std::cout<<edge1<<std::endl;
+  f1->SetLineColor(kBlue);
+  f1->SetLineWidth(3);
+  f1->Draw("same");
 
   TF1 * f2 = new TF1("f2", "[0]*TMath::Erf((x-[1])/[2])+[3]", 14.83, 15.3);
-  f2->SetParameters(1., 15., 1, 0.);
+  f2->SetParameters(1., 15.08, 1, 0.);
   g2->Fit("f2","R");
   Double_t edge2 = f2->GetX(0.5);
+  f2->SetLineColor(kRed);
+  f2->SetLineWidth(3);
+  f2->Draw("same");
 
 
   TArrow *ar3 = new TArrow(edge1,0.5,edge2,0.5,0.05,"<|>");
@@ -161,7 +337,7 @@ void MakeGap_CNM() {
   ar3->SetFillColor(1);
   ar3->Draw();
 
-  TLatex *   tex = new TLatex(0.41, 0.5,Form("%4.2f #mum",1000*(edge2-edge1)));
+  TLatex *   tex = new TLatex(0.50, 0.5,Form("%4.0f#mum",round(100*(edge2-edge1))*10));
   tex->SetNDC();
   tex->SetTextSize(0.05);
   tex->Draw();
@@ -171,24 +347,52 @@ void MakeGap_CNM() {
   tex1->SetTextSize(0.05);
   tex1->Draw();  
   
+  TLegend *leg = new TLegend(0.68,0.35,0.94,0.5,NULL,"brNDC");
+   leg->SetBorderSize(1);
+   leg->SetTextSize(0.04);
+   leg->SetLineColor(1);
+   leg->SetLineStyle(1);
+   leg->SetLineWidth(2);
+   leg->SetFillColor(0);
+   leg->SetFillStyle(1001);
+   leg->AddEntry(g1, "Channel 10", "LP");
+   leg->AddEntry(g2, "Channel 13", "LP");
+   leg->Draw();
+
+   c->SaveAs("Eff_vs_X_CNM_Ch10_13_fit.pdf");
+   c->SaveAs("Eff_vs_X_CNM_Ch10_13_fit.C");
+
 }
 
-void MakeGap_CNMY() {
+void MakeGap_CNMVsY() {
 
-  TFile *_file1 = TFile::Open("Eff_vs_Y_Ch13.root");
-  TFile *_file2 = TFile::Open("Eff_vs_Y_Ch12.root");
+  TFile *_file1 = TFile::Open("eff_Channel13_Run838+839+842.root");
+  TFile *_file2 = TFile::Open("eff_Channel12_Run838+839+842.root");
   
-  TCanvas *c = new TCanvas("c","c",700,600);
+  TCanvas *c = new TCanvas("c","c",800,800);
   c->SetLeftMargin(0.13);
-  c->SetRightMargin(0.18);
+  c->SetRightMargin(0.05);
 
-  TCanvas *c1 = (TCanvas*)_file1->Get("canvas");
-  TCanvas *c2 = (TCanvas*)_file2->Get("canvas");
-  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)c1->GetPrimitive("Graph0");
-  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)c2->GetPrimitive("Graph0");
+  TGraphAsymmErrors *g1 = (TGraphAsymmErrors*)_file1->Get("Efficiency_Y");
+  TGraphAsymmErrors *g2 = (TGraphAsymmErrors*)_file2->Get("Efficiency_Y");
 
-  g2->SetMarkerColor(2);
-  g2->SetLineColor(2);
+
+  g1->SetMarkerSize(1.2);
+  g2->SetMarkerSize(1.2);
+  g1->SetMarkerColor(kBlue);
+  g1->SetLineColor(kBlue);
+  g2->SetMarkerColor(kRed);
+  g2->SetLineColor(kRed);
+  g1->SetLineWidth(2);
+  g2->SetLineWidth(2);
+  
+  g1->GetXaxis()->SetLabelSize(0.04);
+  g1->GetXaxis()->SetTitleSize(0.04);
+  g1->GetXaxis()->SetLabelOffset(0.02);
+  g1->GetXaxis()->SetTitleOffset(1.2);
+  g1->GetYaxis()->SetLabelSize(0.05);
+  g1->GetYaxis()->SetTitleSize(0.05);
+  g1->GetYaxis()->SetTitleOffset(1.1);
 
   // gStyle->SetPalette(1); //kBird
 
@@ -199,16 +403,22 @@ void MakeGap_CNMY() {
   g2->Draw("p same");
   
   TF1 * f1 = new TF1("f1", "[0]*TMath::Erf(([1]-x)/[2])+[3]", 22.3, 22.9);
-  f1->SetParameters(1., 22.6, 1, 0.);
+  f1->SetParameters(1., 23.0, 1, 0.);
   g1->Fit("f1","R");
 
   Double_t edge1 = f1->GetX(0.5);
   std::cout<<edge1<<std::endl;
+  f1->SetLineColor(kBlue);
+  f1->SetLineWidth(3);
+  f1->Draw("same");
 
   TF1 * f2 = new TF1("f2", "[0]*TMath::Erf((x-[1])/[2])+[3]", 22.32, 22.88);
   f2->SetParameters(1., 22.6, 1, 0.);
   g2->Fit("f2","R");
   Double_t edge2 = f2->GetX(0.5);
+  f2->SetLineColor(kRed);
+  f2->SetLineWidth(3);
+  f2->Draw("same");
 
 
   TArrow *ar3 = new TArrow(edge1,0.5,edge2,0.5,0.05,"<|>");
@@ -219,7 +429,7 @@ void MakeGap_CNMY() {
   ar3->SetFillColor(1);
   ar3->Draw();
 
-  TLatex *   tex = new TLatex(0.41, 0.5,Form("%4.2f #mum",1000*(edge2-edge1)));
+  TLatex *   tex = new TLatex(0.47, 0.5,Form("%4.0f#mum",round(100*(edge2-edge1))*10));
   tex->SetNDC();
   tex->SetTextSize(0.05);
   tex->Draw();
@@ -228,8 +438,31 @@ void MakeGap_CNMY() {
   tex1->SetNDC();
   tex1->SetTextSize(0.05);
   tex1->Draw();  
+
+  TLegend *leg = new TLegend(0.68,0.35,0.94,0.5,NULL,"brNDC");
+   leg->SetBorderSize(1);
+   leg->SetTextSize(0.04);
+   leg->SetLineColor(1);
+   leg->SetLineStyle(1);
+   leg->SetLineWidth(2);
+   leg->SetFillColor(0);
+   leg->SetFillStyle(1001);
+   leg->AddEntry(g1, "Channel 13", "LP");
+   leg->AddEntry(g2, "Channel 12", "LP");
+   leg->Draw();
+
+   c->SaveAs("Eff_vs_Y_CNM_Ch13_12_fit.pdf");
+   c->SaveAs("Eff_vs_Y_CNM_Ch13_12_fit.C");
   
 }
 
 
 
+void plotLGADGaps () {
+
+  MakeGap_HPKVsX();
+  MakeGap_HPKVsY();
+  MakeGap_CNMVsX();
+  MakeGap_CNMVsY();
+
+}
